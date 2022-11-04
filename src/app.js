386d40884ab846e37976cf3ee49936d5eb8bf251
Paurnima <rajputp2007@gmail.com>
2022-11-04 23:36:49 +0530

    UI created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 const app = express()
 const publicDirectory = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
 app.set('view engine', 'hbs')
-
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
 app.use(express.static(publicDirectory))
-
 app.get('', (req, res) => {
     res.render('index')
 })
@@ -15,12 +19,14 @@ app.get('/login', (req, res) => {
 app.get('/forget-password', (req, res) => {
     res.render('forget-password')
 })
-
 app.get('/reset-password', (req, res) => {
     res.render('reset-password')
 })
 app.get('/signup', (req, res) => {
     res.render('signup')
+})
+app.get('/home', (req, res) => {
+    res.render('index')
 })
 app.post('/home/generate_URL', (req, res) => {
     res.send("generate url")
